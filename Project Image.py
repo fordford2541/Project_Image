54589a77f2c80b52157092a38ab4406dfef9b488
phasapol <p_or_987@hotmail.com>
2020-01-13 18:40:38 +0700

    ลบอันที่ไม่เกี่ยว

--- Project Image.py
+++ Project Image.py
@@ -1,3 +1,4 @@
 import cv2
-img = cv2.imread("IMG_20190912_120749.jpg")
-cv2.imshow("img",img)
+img = cv2.imread("/Users/por/Documents/Project_Image/IMG_20190912_120802.jpg")
+cv2.imshow("image",img)
+cv2.waitKey()
